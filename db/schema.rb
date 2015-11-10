a70ccac8ac84a4e0a9a1b4aad554412dbd8e0059
Graeme <flangemodo@hotmail.com>
2015-11-10 11:30:46 +0000

    enable authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110110143) do
+ActiveRecord::Schema.define(version: 20151110112556) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "player1_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20151110110143) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
