ac1d1af7ddf5ccba8f2fbdbe28310aad9915cbb9
Graeme <flangemodo@hotmail.com>
2015-11-11 19:07:55 +0000

    add humour

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Game.delete_all
 User.delete_all
-User.create!(email: 'flangemodo@hotmail.com', name: 'Graeme Stewart', remote_avatar_url: 'http://pre10.deviantart.net/23dd/th/pre/i/2013/107/e/0/ryu_street_fighter_ii_sprite_hd_by_tyller16-d620tp0.png', role: 'Admin', password: '12345678')
-User.create!(email: 'iamjcvd@hotmail.com', name: 'Jean-Claude Van-Damme', remote_avatar_url: 'http://cdn.wikimg.net/strategywiki/images/a/a4/SSF2T_Ken.gif', role: 'Admin', password: '12345678')
+User.create!(email: 'flangemodo@hotmail.com', name: 'Graeme Stewart', remote_avatar_url: 'http://vignette4.wikia.nocookie.net/streetfighter/images/c/cb/Ryu-hdstance.gif/revision/latest?cb=20100718231721', role: 'Admin', password: '12345678')
+User.create!(email: 'iamjcvd@hotmail.com', name: 'Jean-Claude Van-Damme', remote_avatar_url: 'http://33.media.tumblr.com/tumblr_m2ewpdPGXI1rptf33o1_500.gif', role: 'Admin', password: '12345678')
 Game.create!(player1: User.first, player2: User.last)
