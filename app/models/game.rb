6441051596208cecfc1375c17e806dd7cb03d67b
Graeme <flangemodo@hotmail.com>
2015-11-11 13:29:19 +0000

    additional validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,10 @@ class Game < ActiveRecord::Base
   WINNING_LINES = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
   def make_move(player, square)
+    if player == whose_turn && square != Move.offset(1).last.square then
         Move.create(square: square, symbol: symbol_for_player(player), player: player, game: self)
+    else 
+    end
         save!
   end
 
