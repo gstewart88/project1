0014ce8522b624218df0ef4ab94e246f9a03a854
Graeme <flangemodo@hotmail.com>
2015-11-11 14:22:47 +0000

    12th commit

--- app/models/move.rb
+++ app/models/move.rb
@@ -4,6 +4,7 @@ class Move < ActiveRecord::Base
 
   validate :square_is_empty
   validate :is_players_turn
+  validate :is_game_over
 
 
   private
@@ -16,6 +17,9 @@ class Move < ActiveRecord::Base
     errors.add(:base, "it's not your turn") unless game.is_players_turn?(player)
   end
 
-  
+  private
+  def is_game_over
+    errors.add(:base, "The game has finished. Move on. It's over.") unless !game.finished?
+  end
 
 end
